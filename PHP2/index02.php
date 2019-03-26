75efc13c8f2d62ac24137464f852ed37d1875378
GiedreMila <giedre.mila@gmail.com>
2019-03-26 16:45:57 +0200

    pateiktas namu darbas PHP2

---
+++ PHP2/index02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP2</title>
+</head>
+
+<body>
+
+    <?php
+
+       $city = 'Vilnius';
+
+       $county = 'Lietuva';
+
+       $pupulation = 574221;
+
+       $largest_population = true;
+    ?>
+
+    <?php echo $city. ' - Lietuvos sostinė';?>
+
+    <?php echo "$city ir $county sparčiai auga.";?>
+
+</body>
+
+</html>
+
