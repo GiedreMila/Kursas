1a7e50c854d3281460fc0dfd238b8e234dca0eea
GiedreMila <giedre.mila@gmail.com>
2019-03-26 16:46:29 +0200

    pateiktas namu darbas PHP3

---
+++ PHP3/index03.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP03</title>
+</head>
+
+<body>
+
+    <?php
+
+     $x = 10;
+     $y = 7;
+     $z = $x + $y;
+
+    ?>
+
+       <?php echo "$x  +  $y = $z";?><br>
+
+        <?php $z = $x - $y;?>
+
+       <?php echo "$x - $y = $z";?><br>
+
+        <?php $z = $x * $y;?>
+
+       <?php echo "$x * $y = $z";?><br>
+
+        <?php $z = $x / $y;?>
+
+       <?php echo "$x / $y =$z ";?><br>
+
+        <?php $z = $x % $y;?>
+
+       <?php echo "$x % $y = $z";?><br>
+
+</body>
+
+</html>
